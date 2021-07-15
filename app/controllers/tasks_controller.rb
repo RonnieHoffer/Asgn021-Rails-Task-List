f9cb273334ba7e2496a45bcbe835e15dc4aed745
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-07-15 19:30:16 -0400

    App is now functional, except for the delete feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,15 +2,32 @@ class TasksController < ApplicationController
     
     def index
         @tasks = Task.all
+        @task = Task.new
     end
 
     def new
+        @task = Task.new
     end
 
     def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to tasks_url
+        else
+            render :new
+        end
     end
     
     def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_url
+    end
+
+
+    private
+    def task_params
+        params.require(:task).permit(:name)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'tasks' => 'tasks#index'
+  # get 'tasks' => 'tasks#index'
   
-  # resources :tasks
+  root 'tasks#index'
+  resources :tasks
+  
 end
